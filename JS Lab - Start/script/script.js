b37e44b452290f463400f6e605906129aab7b994
SuperSpencer2004 <spencer.gosselin@icloud.com>
2024-11-15 12:29:35 -0800

    first commit

---
+++ JS Lab - Start/script/script.js
@@ -0,0 +1,24 @@
+function addValue(){
+    // get the operands value
+
+    // parse to integer. You can use: parseInt(input, 10);
+
+    // perform operation
+
+    // creating the text output
+
+    // (if you want to check) print out in console
+
+    // change the text area
+
+    document.mycalculator.output.value = text;
+}
+
+
+function mulValue(){
+    document.mycalculator.output.value = text;
+}
+
+function divValue(){
+    document.mycalculator.output.value = text;
+}
